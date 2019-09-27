078efe4e7cdf49209ac078fee1aa60dcaefe53ab
JJingtang <48779734+jjingtang@users.noreply.github.com>
2019-09-27 22:44:38 +1000

    add generate random bricks

---
+++ Assets/Script/brickManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brickManager : MonoBehaviour
+{
+    public GameObject[] bricks; //store all types of bricks in the array
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateNewBrick();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void generateNewBrick()
+    {
+        //generate a new brick from the array (7 types of bricks) by using a random number to pick a random type of brick and initate at the empty object "newBrick" position 
+        Instantiate(bricks[Random.Range(0, bricks.Length)], transform.position, Quaternion.identity);
+    }
+}
--- Assets/Script/brickMovement.cs
+++ Assets/Script/brickMovement.cs
@@ -22,30 +22,30 @@ public class brickMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) //if pressing the up arrow or w, the game object will rotate
         {
             //transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-            transform.Rotate(0, 0, 90, Space.World);
+            transform.Rotate(0, 0, 90, Space.World); //it needs to rotate in space world, otherwise it will be messy
             if (!isHitBorder())
             {
-                transform.Rotate(0, 0, -90, Space.World);
+                transform.Rotate(0, 0, -90, Space.World); //if it hit the border, then rotate back to ensure it won't hit the border
                 //transform.RotateAround(transform.TransformPoint(rotationPoint),new Vector3(0,0,1), -90);
             }
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) //if pressing the left arrow or a, the game object will move left (x-axis)
         {
             transform.Translate(-1, 0, 0, Space.World);
             if (!isHitBorder())
                 transform.Translate(1, 0, 0, Space.World);
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) //if pressing the right arrow or d, the game object will move right (x-axis)
         {
             transform.Translate(1, 0, 0, Space.World);
             if (!isHitBorder())
                 transform.Translate(-1, 0, 0,Space.World);
         }
 
-        if(Time.time - lastTime > (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ? 0.5f / 5: 0.5f))
+        if(Time.time - lastTime > (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ? 0.5f / 5: 0.5f)) //if pressing the down arrow or s, the game object will drop down quickly
         {
             transform.Translate(0, gravity, 0,Space.World);
             lastTime = Time.time;
@@ -61,8 +61,8 @@ public class brickMovement : MonoBehaviour
     {
         foreach(Transform children in transform)
         {
-            int x = Mathf.RoundToInt(children.transform.position.x);
-            int y = Mathf.RoundToInt(children.transform.position.y);
+            int x = Mathf.RoundToInt(children.transform.position.x); //get the x of the gameObject
+            int y = Mathf.RoundToInt(children.transform.position.y); //get the y of the gameObject
             if (x<=0 || x >= borderX || y<=0|| y >= borderY)
             {
                 return false;
